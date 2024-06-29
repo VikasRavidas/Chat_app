f3883da68a299236dfb7bc3c21c9c6f274c29efd
Vikas  Ravidas <126586254+Vikas123eng@users.noreply.github.com>
2024-06-29 18:26:01 +0530

    Login UI Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { fetchPosts } from '../actions/posts';
-import PostsList from './PostsList';
 import Navbar from './Navbar';
-
-const Login = () => <div>Login</div>;
+import Page404 from './Page404';
+import Home from './Home';
+import Login from './Login';
 
 const Signup = () => <div>Signup</div>;
-
-const Home = () => <div>Home</div>;
 
 class App extends React.Component {
   componentDidMount() {
@@ -23,22 +21,12 @@ class App extends React.Component {
       <Router>
         <div>
           <Navbar />
-          <PostsList posts={posts} />
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/signup">Signup</Link>
-            </li>
-          </ul>
+
           <Routes>
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
+            <Route exact path="/" element={<Home posts={posts} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<Page404 />} />
           </Routes>
         </div>
       </Router>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,15 @@ import React from 'react';
 import gif from './img/Tech4.gif';
 import images from './img/images.png';
 import search_icon from './img/search_icon.png';
+import { Link } from 'react-router-dom';
 
 function Navbar(props) {
   return (
     <nav className="nav">
       <div className="left-div">
-        <img src={gif} alt="logo" />
+        <Link to={'/'}>
+          <img src={gif} alt="logo" />
+        </Link>
       </div>
       <div className="search-container">
         <img className="search-icon" src={search_icon} alt="search-icon" />
@@ -33,9 +36,15 @@ function Navbar(props) {
         </div>
         <div className="nav-links">
           <ul>
-            <li>Log in</li>
-            <li>Log out</li>
-            <li>Register</li>
+            <li>
+              <Link to="/login">Log in</Link>
+            </li>
+            <li>
+              <Link to="/logout">Log out</Link>
+            </li>
+            <li>
+              <Link to="/signup">Register</Link>
+            </li>
           </ul>
         </div>
       </div>
